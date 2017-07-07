5433ae2f6bd46412d26df1764098f1ba55f70c5f
Dylan Hyatt-Denesik <hyattden@ualberta.ca>
2017-07-06 19:49:08 -0600

    comments and absolute path

--- email_script.py
+++ email_script.py
@@ -1,18 +1,24 @@
 import smtplib
+import os
 from email.mime.text import MIMEText
 
+#find email body in text file
 daily_schedule = ''
-file = open("C:\\Users\\Dylan\\Dropbox\\schedule.txt",'r')
+file = open(os.getcwd() + "\\schedule.txt",'r')
 msg = MIMEText(file.read())
 file.close()
 
+#set up subject line
 msg['Subject'] = "Things To Do"
 
+#do SMTP mumbo jumbo
 smtpObj = smtplib.SMTP('smtp.gmail.com',587)
 smtpObj.ehlo()
 smtpObj.starttls()
 smtpObj.login('python.hyatt@gmail.com','command and')
 value = smtpObj.sendmail('hyatt.denesik@gmail.com','hyatt.denesik@gmail.com',msg.as_string())
+
+#if value dictionary not empty try again, indicates failure
 if value:
     value = smtpObj.sendmail('hyatt.denesik@gmail.com','hyatt.denesik@gmail.com',msg.as_string())
 smtpObj.quit()
